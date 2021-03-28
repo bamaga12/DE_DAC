b09a127f9c6688da4baabf7b4bd4397cc7600669
unknown <kythuat7_igitech@git.igisoft.vn>
2021-03-28 23:30:23 +0700

    huyenpt36 nop bai tuan 3

---
+++ src/tuan3/ConvertingArraysOfStringsToString.scala
@@ -0,0 +1,24 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, concat_ws}
+
+object ConvertingArraysOfStringsToString {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().master("local[1]").appName("SparkPractice").getOrCreate()
+    import spark.implicits._
+
+    spark.sparkContext.setLogLevel("WARN")
+
+
+    val words = Seq(Array("hello", "world")).toDF("words")
+    //    words.show
+    //    words.printSchema
+
+    val solution = words.withColumn("solution", concat_ws(" ", $"words"))
+
+    solution.show(false)
+    solution.printSchema
+
+  }
+}
---
+++ src/tuan3/DifferenceInDaysBetweenDatesAsStrings.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, current_date, datediff, to_date, unix_timestamp}
+
+object DifferenceBetweenDatesAsStrings {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[1]").appName("SparkPractice").getOrCreate()
+    import spark.implicits._
+    spark.sparkContext.setLogLevel("WARN")
+
+    val dates = Seq(
+      "08/11/2015",
+      "09/11/2015",
+      "09/12/2015").toDF("date_string")
+    //    dates.show
+
+    //    dates.withColumn("date_string", to_date($"date_string")).show
+    //    val result = dates.withColumn("date_string", to_date($"date_string", "dd/MM/yyyy").alias("to_date"))
+    //    val result = dates.withColumn("date_string", datediff(current_date(),to_date($"date_string", "dd/MM/yyyy")).as("datediff"))
+
+    val result = dates.select(col("date_string"), to_date($"date_string", "dd/MM/yyyy").alias("to_date"), datediff(current_date(),to_date($"date_string", "dd/MM/yyyy")).as("datediff"))
+    result.show()
+
+  }
+}
